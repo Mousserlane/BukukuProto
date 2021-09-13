dc34765d4818e693f7cdcdb2030bf4ce16dba060
Arga Aditya <argaaditya@gmail.com>
2021-09-13 19:20:39 +0700

    [ADD]: Card component

---
+++ src/components/Card.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Image, Text, TouchableOpacity } from "react-native";
+import { Box } from "./generic";
+
+interface CardProps {
+  id: string;
+  title: string;
+  coverImage: string;
+  publishedDate: Date;
+  subtitle?: string;
+  ageGroup?: string;
+}
+
+export const Card = (props: CardProps) => {
+  const { id, title, coverImage } = props;
+  const navigateToDetail = () => {
+    // TODO : Add navigation
+    // navigation.navigate(RouteNames.bookDetail, {id, title, subtitle, coverImage, ageGroup})
+  };
+
+  return (
+    <TouchableOpacity onPress={navigateToDetail}>
+      <Image
+        source={{ uri: coverImage }}
+        width={150}
+        height={188}
+        style={{ width: 150, height: 188, borderRadius: 8 }}
+      />
+    </TouchableOpacity>
+  );
+};
