54e6345da8634578b43be442b2d5c695b57547ea
Arga Aditya <argaaditya@gmail.com>
2021-09-15 15:43:50 +0700

    [FIX]: Navigation error

--- src/screens/MainScreens.tsx
+++ src/screens/MainScreens.tsx
@@ -10,19 +10,17 @@ const MainScreens = () => {
   return (
     <Stack.Navigator>
       <Stack.Group>
-        <Stack.Group>
-          <Stack.Screen
-            name={RouteNames.MainNavigator}
-            component={MainTabs}
-            options={{
-              headerShown: false
-            }}
-          />
-          <Stack.Screen
-            component={BookDetailScreen}
-            name={RouteNames.BookDetailScreen}
-          />
-        </Stack.Group>
+        <Stack.Screen
+          name={RouteNames.MainNavigator}
+          component={MainTabs}
+          options={{
+            headerShown: false
+          }}
+        />
+        <Stack.Screen
+          component={BookDetailScreen}
+          name={RouteNames.BookDetailScreen}
+        />
       </Stack.Group>
     </Stack.Navigator>
   );
