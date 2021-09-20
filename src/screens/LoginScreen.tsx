dd154f98d0b15cee45a5b53eb04faf84f69670e9
Arga Aditya <argaaditya@gmail.com>
2021-09-20 19:27:42 +0700

    [ADD]: Login Screen

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,15 +1,55 @@
 import React from "react";
-import { Text } from "react-native";
-import { Box } from "../components/generic";
+import { Box, Button, InputBox, Text } from "../components/generic";
 import RouteNames from "../RouteNames";
+import { colors, spaces } from "../styles/styles";
 
 const LoginScreen = ({ navigation }: { navigation: any }) => {
+  const logIn = () => {
+    navigation.navigate(RouteNames.MainNavigator);
+  };
+
   const navigateToRegistrationScreen = () => {
     navigation.navigate(RouteNames.RegistrationScreen);
   };
+
   return (
-    <Box flex={1} justifyContent="center" alignItems="center">
-      <Text onPress={navigateToRegistrationScreen}>Register?</Text>
+    <Box flex={1} backgroundColor={colors.accent} py="7" px="7">
+      <Box flex={1} justifyContent="flex-end">
+        <Text.H1>Selamat datang kembali!</Text.H1>
+
+        {/** TODO : These Boxes can be replaced with Text margin options */}
+        <Box mt="7" mb="5">
+          <Text.H3>Email</Text.H3>
+          <InputBox
+            placeholder="Email anda"
+            style={{ marginVertical: spaces["3"] }}
+          />
+        </Box>
+
+        <Box width="100%">
+          <Text.H3>Password</Text.H3>
+          <InputBox
+            placeholder="Password"
+            style={{ marginVertical: spaces["3"] }}
+            secureTextEntry
+          />
+        </Box>
+
+        <Text.Body>
+          Belum punya akun?{" "}
+          <Text.Body
+            onPress={navigateToRegistrationScreen}
+            style={{ fontWeight: "700" }}
+          >
+            Registrasi
+          </Text.Body>
+        </Text.Body>
+      </Box>
+      <Box flex={0.2} justifyContent="flex-end">
+        <Button type="primary" size="l" block onPress={logIn}>
+          Masuk
+        </Button>
+      </Box>
     </Box>
   );
 };
