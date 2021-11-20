4992db3b1164d8125806d18e891139ebdf49a69e
Arga Aditya <argaaditya@gmail.com>
2021-11-20 20:04:55 +0700

    [ADD] icons for iOS

--- src/screens/MainTabs.tsx
+++ src/screens/MainTabs.tsx
@@ -4,8 +4,14 @@ import HomeScreen from "./HomeScreen";
 import RouteNames from "../RouteNames";
 import BookmarkScreen from "./BookmarkScreen";
 import SettingScreen from "./SettingsScreen";
+import Icon from "react-native-vector-icons/Entypo";
 
 const Tab = createBottomTabNavigator();
+
+const HomeIcon = () => <Icon name="home" size={24} />;
+const BookmarkIcon = () => <Icon name="bookmark" size={24} />;
+// const SearchIcon = () => <Icon name="magnifying-glass" size={24} />;
+const SettingIcon = () => <Icon name="cog" size={24} />;
 
 const MainNavigator = () => {
   return (
@@ -13,17 +19,17 @@ const MainNavigator = () => {
       <Tab.Screen
         name={RouteNames.HomeScreen}
         component={HomeScreen}
-        options={{ tabBarLabel: "Home" }}
+        options={{ tabBarLabel: "Home", tabBarIcon: HomeIcon }}
       />
       <Tab.Screen
         name={RouteNames.BookmarkScreen}
         component={BookmarkScreen}
-        options={{ tabBarLabel: "Bookmark" }}
+        options={{ tabBarLabel: "Bookmark", tabBarIcon: BookmarkIcon }}
       />
       <Tab.Screen
         name={RouteNames.SettingsScreen}
         component={SettingScreen}
-        options={{ tabBarLabel: "Settings" }}
+        options={{ tabBarLabel: "Settings", tabBarIcon: SettingIcon }}
       />
     </Tab.Navigator>
   );
